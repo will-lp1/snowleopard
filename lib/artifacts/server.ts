ce749c7f27c9cfa7ad1a38f006e03fa6f6b124bb
will-lp1 <dlasycorporate@gmail.com>
2025-03-13 19:15:51 +0000

    Update server.ts

--- lib/artifacts/server.ts
+++ lib/artifacts/server.ts
@@ -1,6 +1,3 @@
-import { codeDocumentHandler } from '@/artifacts/code/server';
-import { imageDocumentHandler } from '@/artifacts/image/server';
-import { sheetDocumentHandler } from '@/artifacts/sheet/server';
 import { textDocumentHandler } from '@/artifacts/text/server';
 import { ArtifactKind } from '@/components/artifact';
 import { DataStreamWriter } from 'ai';
