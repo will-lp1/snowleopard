8e71f270df171781ef216283cbf4e1f144006ed4
will-lp1 <dlasycorporate@gmail.com>
2025-04-22 11:19:17 +0100

    fix: login/register width

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -52,14 +52,14 @@ export default function LoginPage() {
   return (
     <div className="flex h-dvh w-screen items-start pt-12 md:pt-0 md:items-center justify-center bg-background">
       <div className="w-full max-w-md overflow-hidden rounded-2xl flex flex-col gap-12">
-        <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
+        <div className="flex flex-col items-center justify-center gap-2 px-8 text-center">
           <h3 className="text-xl font-semibold dark:text-zinc-50">Sign In</h3>
           <p className="text-sm text-gray-500 dark:text-zinc-400">
             Sign in with your email and password
           </p>
         </div>
         
-        <div className="px-4 sm:px-16">
+        <div className="px-8">
           <AuthForm action={handleEmailLogin} defaultEmail={email}> 
             <SubmitButton 
               isSuccessful={isEmailSuccessful}
