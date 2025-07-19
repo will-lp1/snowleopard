81aa7ee36e52e4ed4ce1a4b9b42a95acb2b4d4d9
will-lp1 <dlasycorporate@gmail.com>
2025-07-19 14:46:01 -0700

    Update sitemap.ts

--- apps/snow-leopard/app/sitemap.ts
+++ apps/snow-leopard/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.VERCEL_URL || 'https://www.cursorforwrit.ing'
+  const baseUrl = 'https://www.cursorforwrit.ing'
   
   const staticRoutes = [
     {
